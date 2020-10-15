285ff2f4eae3f1744616ec50c4802db5733c1c29
wojciechgula <gulasz101@gmail.com>
2020-10-15 21:49:36 +0200

    Adding properties to model.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,19 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Product
  * @package App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $price
+ * @property CarbonInterface $created_at
+ * @property CarbonInterface $updated_at
  */
 class Product extends Model
 {
