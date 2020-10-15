cc69d48bb488f9d62a8357101482f0c2702cbe8f
wojciechgula <gulasz101@gmail.com>
2020-10-15 21:01:56 +0200

    Introducing "dummy" products endpoint.

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+
+/**
+ * Class ProductsController
+ * @package App\Http\Controllers
+ */
+class ProductsController extends Controller
+{
+
+    public function store()
+    {
+
+    }
+}
